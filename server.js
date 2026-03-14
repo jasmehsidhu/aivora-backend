3366b3839a79d509d96edbe8e9b86c0181d4ed34
Jasmeh Singh <142116700+jasmehsidhu@users.noreply.github.com>
2026-03-14 11:00:21 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors'
+import OpenAI from "openai";
+import axios from 'axios'
+const app=express()
+const PORT=1000
+app.use(cors())
+app.use(express.json())
+app.listen(PORT,()=>{
+    console.log('Server Started')
+})
+
+
+app.post('/request',async(req,res)=>{
+ var response=await axios.post("https://chatgpt-42.p.rapidapi.com/conversationgpt4-2",
+     {
+    messages: [
+      { role: "user", content: req.body.message }
+    ],
+    system_prompt: "",
+    temperature: 0.9,
+    top_k: 5,
+    top_p: 0.9,
+    max_tokens: 256,
+    web_access: false
+  },{
+     headers: {
+      "x-rapidapi-key": "1b6d70030dmshf9306ff20c8f459p1b46fajsn53e25d199006",
+      "x-rapidapi-host": "chatgpt-42.p.rapidapi.com",
+      "Content-Type": "application/json"
+    }
+  }
+   )
+    res.json({response:response.data.result})
+})
